df471116e75dd0d7a07c5f21a7c11f4d1b2937bd
Jenkins <jenkins@deeson.co.uk>
2021-03-22 13:23:05 +0000

    Deploying 9edeff5a5f888bdad39a45b3a85ab2a233897ab1: Merged in feature/DEV-433-content-carousel (pull request #452)
    feature/DEV-433-content-carousel
    
    Approved-by: Peter Briers

---
+++ components/raw/content-carousel/content-carousel.js
@@ -0,0 +1,60 @@
+import $ from 'jquery'
+import 'owl.carousel';
+
+let carouselContainers = document.querySelectorAll('.carousel');
+
+for (let i = 0; i < carouselContainers.length; i++) {
+  let owlClass = `owl-carousel-${i}`
+  let navId = `nextSlide-${i}`;
+
+  let navButton = `<a href="" id="${navId}" class="carousel-control"><i class="uod-icons uod-icons-chevron-right"></i></a>`
+  carouselContainers[i].insertAdjacentHTML('afterend', navButton);
+
+  let carousel = carouselContainers[i].querySelector('.owl-carousel');
+  carousel.classList.add(owlClass);
+
+let owl = $('.' + owlClass).owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1.3,
+        nav: false,
+        loop: true,
+        margin: 10
+      },
+      900: {
+        items: 2.8,
+        nav: false,
+        loop: true,
+        margin: 20,
+      },
+      1200: {
+        items: 1.8,
+        nav: false,
+        loop: true,
+        margin: 20,
+      },
+      1380: {
+        items: 2.8,
+        nav: false,
+        loop: true,
+        margin: 20,
+      }
+    }
+  })
+
+  $(`#${navId}`).click((e) => {
+    e.preventDefault();
+    owl.trigger('next.owl.carousel');
+  })
+}
+
+
+/*$("#next").click((e) => {
+  e.preventDefault();
+  owl.trigger('next.owl.carousel');
+})*/
--- ui.js
+++ ui.js
@@ -60,22 +60,22 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 67);
+/******/ 	return __webpack_require__(__webpack_require__.s = 70);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 67:
+/***/ 70:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(68);
+__webpack_require__(71);
 
 /***/ }),
 
-/***/ 68:
+/***/ 71:
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
